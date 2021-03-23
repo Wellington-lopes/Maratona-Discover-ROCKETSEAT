40370264538c677e89cfeba84ba3a0e9772a0d15
Wellington Lopes <67521652+Wellington-lopes@users.noreply.github.com>
2021-03-23 14:59:28 -0300

    Trabalhando JS
    Trabalhando com o Js

--- Maratona-discover-Rocketseat/script.js
+++ Maratona-discover-Rocketseat/script.js
@@ -17,3 +17,54 @@ const Modal = {
      }
  }
 
+const transactions = [
+    {
+        id: 1,
+        description: 'Luz',
+        amount: -50000,
+        date: '23/01/2021',
+    }, 
+    {
+        id: 2,
+        description: 'Website',
+        amount: 500000,
+        date: '23/01/2021',
+    }, 
+    {
+        id: 3,
+        description: 'Internet',
+        amount: -20000,
+        date: '23/01/2021',
+    }, 
+
+];
+ 
+ const Transaction = {
+     income() {
+         //somar as entradas
+     },
+     expenses() {
+         //somar as saídas
+     }
+     total() {
+         //Entradas - saídas
+     }
+ }
+
+ //document object model
+const DOM = {
+    innerHTMLTransaction() {
+
+        const html = `
+            <tr>
+            <td class="description">Luz</td>
+            <td class="expense">- R$ 500,00</td>
+            <td class="date">22/01/2021</td>
+            <td>
+                <img src="./assets/minus.svg" alt="remover transação">
+            </td>
+            </tr>
+        `
+
+    }
+}
